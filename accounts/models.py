453257d03c923449b46ea53a1a338d59d319fae9
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-10-08 15:54:22 +0530

    customers and orders logic, done

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null= True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     CATEGORY = (
@@ -19,21 +25,19 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null= True)
     price = models.FloatField(null= True)
     category = models.CharField(max_length=200, null= True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null= True)
+    description = models.CharField(max_length=200, null= True, blank=True)
     date_created = models.DateTimeField(auto_now_add = True, null= True)
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null= True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
 
 
+
 class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
-        ('Out of Delivery', 'Out of Delivery'),
+        ('Out for Delivery', 'Out for Delivery'),
         ('Delivered','Delivered'),
     )
     #customer and product to be added
@@ -41,5 +45,11 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null = True, on_delete = models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add = True, null= True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
+
+
+    def __str__(self):
+        return self.product.name
+
+    
+    
 
