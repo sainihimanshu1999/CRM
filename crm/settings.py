3bfb03b60098d686423ffc8ac960554c757c23d8
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-10-14 15:24:16 +0530

    done

--- crm/settings.py
+++ crm/settings.py
@@ -76,14 +76,21 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'DEMO_TEST',
+#         'USER':'postgres',
+#         'PASSWORD':'Himanshu9716',
+#         'HOST':'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER':'postgres',
-        'PASSWORD':'Himanshu9716',
-        'HOST':'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
